2969e450a46dcb62a07a6a53d26aa1864ffc7a0b
muchammad-athfal <127067307+muchammad-athfal@users.noreply.github.com>
2023-09-30 01:23:54 +0700

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+type ButtonProps = {
+  variant: string;
+};
+const Button = ({ variant }: ButtonProps) => {
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md bg-${variant}-700 text-white`}
+      type="submit"
+    >
+      Button
+    </button>
+  );
+};
+
 function App() {
   return (
     <>
       <div className="flex justify-center bg-blue-500 min-h-screen items-center">
-        <div>Hello World</div>
+        <Button variant={'red'} />
       </div>
     </>
   );
